9f8ee81e60c16ee59577e4ef0d37c214ef29d144
Wielton <grantwielgosz@gmail.com>
2022-10-10 12:22:47 -0600

    problem with link redirects fixed

--- src/components/ProjectsComp.vue
+++ src/components/ProjectsComp.vue
@@ -11,8 +11,8 @@
                 <v-col cols="12" sm="12" md="6" lg="6">
                     <h1>{{project.name}}</h1>
                     <p class="description-p">{{project.description}}</p>
-                    <v-btn rounded text color="rgb(245,245,245)" elevation="10" class="btn" :to="project.gitLinkFE" target="_blank">Frontend Code</v-btn>
-                    <v-btn rounded text color="rgb(245,245,245)" elevation="10" class="btn" :to='project.gitLinkBE' target="_blank">Backend Code</v-btn>
+                    <a :href="project.gitLinkFE" target="_blank"><v-btn rounded text color="rgb(245,245,245)" elevation="10" class="btn">Frontend Code</v-btn></a>
+                    <a :href="project.gitLinkBE" target="_blank"><v-btn rounded text color="rgb(245,245,245)" elevation="10" class="btn">Backend Code</v-btn></a>
                 </v-col>
                 <v-col cols="12" sm="12" md="6" lg="6" class="d-flex justify-center align-center">
                     <v-img
@@ -34,7 +34,7 @@
         name: "ProjectsComponent",
         data: () => ({
             projects: [
-                {name: 'Pokemon Battle', img: require('../assets/charBattle.png'), gitLinkFE: 'https://github.com/Wielton/Week10Hackathon', description: 'Week 10 Hackathon submission using HTML, CSS, and JavaScript fundamentals.  Upon selection of a Pokemon character, the user is directed to the battle page against a random "evil" character.  The user has four attack options with varying hit values while receiving a counter-attack within the same range.'},
+                {name: 'Pokemon Battle', img: require('../assets/charBattle.png'), gitLinkFE: 'https://github.com/Wielton/Week10Hackathon', gitLinkBE: 'https://github.com/Wielton/Week10Hackathon',description: 'Week 10 Hackathon submission using HTML, CSS, and JavaScript fundamentals.  Upon selection of a Pokemon character, the user is directed to the battle page against a random "evil" character.  The user has four attack options with varying hit values while receiving a counter-attack within the same range.'},
                 {name: 'Foodie: A Skip Clone', img: require('../assets/foodie.png'), gitLinkFE: 'https://github.com/Wielton/project-foodie', gitLinkBE: 'https://github.com/Wielton/foodie-backend', description: 'Just what the title says!  This project was built in two 14-day phases.  The fully-responsive frontend levied the power of Vue, and its framework Vuetify, while using Axios for RESTful API calls. Python/Flask and MariaDB/MySQL handled the backend logic and database queries.'},
                 {name: 'reDraft Fantasy Football', img: require('../assets/redraft-home.png'), gitLinkFE: 'https://github.com/Wielton/redraft-frontend', gitLinkBE: 'https://github.com/Wielton/redraft', description: 'A scalable fantasy football league app consisting of live, turn-based, and timed drafts, roster management, matchup schedule, scoring, and updating rankings.  The current product was my school MVP submission using Vue/Vuetify, Axios, Flask, MariaDB.  User features include the ability to register/join, login/logout, create/join league, add/delete players, and viewing current rosters within the same league.'},
                 
@@ -64,7 +64,9 @@
         color: #b3aaab;
         
     }
-    
+    a {
+        text-decoration: none;
+    }
     .btn {
         background-color: #9d8d8f;
     }
